d644d3c42b4f2b377ea827d52edd5c23c6c03df9
Casey Chance <caseyjchance@gmail.com>
2024-03-31 23:52:23 -0700

    update

--- src/bday_bot/__init__.py
+++ src/bday_bot/__init__.py
@@ -5,6 +5,15 @@ import random
 from dotenv import load_dotenv
 
 from bday_bot.sentiment import analyze_sentiment
+from bday_bot.data import (
+    FACTS_AND_POLLS,
+    KIND_SHELBY_REPLY,
+    REPLY_TO_POSITIVE_RESPONSE,
+    REPLY_TO_NEGATIVE_RESPONSE,
+    REPLY_TO_NEUTRAL_RESPONSE,
+    PATRONIZING_RESPONSE,
+    REACTION_RESPONSES,
+)
 
 load_dotenv()
 
@@ -16,98 +25,6 @@ intents = discord.Intents.none()
 intents.messages = True
 intents.reactions = True
 
-POLLS = [
-    {
-        "type": "poll",
-        "question": "What is Shelby's favorite pizza topping?",
-        "reactions": ["🍕", "🍍", "🍖", "🍄", "🍅"],
-    },
-    {
-        "type": "poll",
-        "question": "Which zoo animal is Shelby's least favorite?",
-        "reactions": ["🦁", "🐘", "🐶", "🐧", "👶"],
-    },
-    {
-        "type": "poll",
-        "question": "If Shelby could eat ice cream, what flavor would they choose?",
-        "reactions": ["🍦", "🍨", "🍧", "🍭"],
-    },
-    {
-        "type": "poll",
-        "question": "FMK: Shelby, Shelbs, Dr. S",
-        "reactions": ["🔥", "💔", "👻"],
-    },
-    {
-        "type": "poll",
-        "question": "Can Shelby literally not even?",
-        "reactions": ["🙅", "🌕", "🙇", "🙈", "👽"],
-    },
-]
-
-
-# Each fact ends with an ironic emoji
-FUN_FACTS = [
-    "Shelby was born with a full set of teeth! 😬",
-    "Shelby has killed and can't wait to kill again 😈",
-    "Shelby once got runner up in a hotdog eating quiz! 🌭",
-    "Shelby has a pet rock named Rocky, a pet stick named Sticky, and a bowl of unknown slime named Scooby Goo! 🐶",
-    "Shelby has a secret talent for juggling the stresses of daily life! 🤹",
-]
-
-FUN_FACT_ITEMS = [
-    {
-        "type": "fact",
-        "text": fact,
-    }
-    for fact in FUN_FACTS
-]
-
-FACTS_AND_POLLS = [*sum(zip(POLLS, FUN_FACT_ITEMS), ())]
-
-KIND_SHELBY_REPLY = [
-    "Happy birthday Shelby!",
-    "Hello Shelby! You're looking great today, I'm sure!",
-    "Hey Shelby! You're doing a great job!",
-    "Hi Shelby! You're the best!",
-    "Hello Shelby! You're amazing!",
-]
-
-PATRONIZING_RESPONSE = [
-    "Shelby, it's nice that you reacted, but this poll isn't about what you think. It's about what everyone else thinks."
-    "Shelby, I know you're excited, but now is not the time to share your opinion."
-    "Shelby, I appreciate your enthusiasm, but let's let others have a chance to share their thoughts."
-    "Shelby, I'm glad you're participating, but let's give others a chance to react too."
-    "Shelby, I know you're eager to share your thoughts, but let's give others a chance to react first."
-]
-
-REPLY_TO_POSITIVE_RESPONSE = [
-    "That's great to hear!",
-    "I'm so happy for you!",
-    "I'm glad you're having a good day!",
-    "That's awesome!",
-]
-
-REPLY_TO_NEGATIVE_RESPONSE = [
-    "That's just what you think.",
-    "Why would you say that?",
-    "I'm sorry?",
-    "Cool I guess...",
-]
-
-REPLY_TO_NEUTRAL_RESPONSE = [
-    "Gotcha",
-    "Uhh...",
-    "Hmm...",
-    "Yeah I guess",
-]
-
-REACTION_RESPONSES = [
-    "Wow, I'll bet Shelby would love to hear you said that",
-    "And I thought Shelby was your friend",
-    "I can't believe you would say that",
-    "Shelby is going to be so mad at you",
-    "I'm sure Shelby would disagree with you",
-]
 
 client = discord.Client(intents=intents)
 
@@ -157,8 +74,8 @@ async def post_fun_fact_or_poll():
             for reaction in item["reactions"]:
                 await message.add_reaction(reaction)
 
-        # Wait for 2 minutes before posting again
-        await asyncio.sleep(30)
+        # Wait for 1.25 hours before posting again
+        await asyncio.sleep(75 * 60)
 
     await channel.send(
         "That's all for now. Thanks for participating! Hope you had a happy birthday Shelby! 🎉🎉🎉"
